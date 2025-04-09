59c696869376290ab2557192f94370cf30f130eb
usaiddd <usaidkhan@gmail.com>
2025-04-09 21:00:27 +0530

    update

--- classes.cpp
+++ classes.cpp
@@ -20,19 +20,21 @@ public:
 class Assets : public tile{
 };
 class Events : public tile{
+public:
+    Events()
 };
 class Property : public Assets{
 private:
     int price;
     int rent;
-    string color;
+    string name;
     bool owned;
     string owner;
 public:
-    Property(int price,int rent,string color){
+    Property(string name,int price,int rent){
         this -> price=price;
         this -> rent=rent;
-        this -> color=color;
+        this -> name=name;
         owned=false;
     }
     void rules(){
@@ -41,11 +43,13 @@ public:
 class Commodity : public Assets{
 private:
     int price;
-    int mortgage;
+    string name;
+    bool owned;
+    string owner;
 public:
-    Commodity(int price,int mortgage){
+    Commodity(string name,int price){
         this -> price=price;
-        this -> mortgage=mortgage;
+        this -> name=name;
     }
     void rules(){
     }
@@ -60,44 +64,44 @@ private:
 
     vector <tile*> board = {
         new Chance("GO"),
-        new Property("Mediterranean Avenue", "Brown", 60, 2),
+        new Property("Mediterranean Avenue", 60, 2),
         new CommunityChest("Community Chest"),
-        new Property("Baltic Avenue", "Brown", 60, 4),
-        new Commodity("Income Tax", "Tax", 200, 0),
-        new Commodity("Reading Railroad", "Railroad", 200, 25),
-        new Property("Oriental Avenue", "Light Blue", 100, 6),
+        new Property("Baltic Avenue", 60, 4),
+        new Commodity("Income Tax", 200, 0),
+        new Commodity("Reading Railroad", 200, 25),
+        new Property("Oriental Avenue", 100, 6),
         new Chance("Chance"),
-        new Property("Vermont Avenue", "Light Blue", 100, 6),
-        new Property("Connecticut Avenue", "Light Blue", 120, 8),
+        new Property("Vermont Avenue", 100, 6),
+        new Property("Connecticut Avenue", 120, 8),
         new Chance("Jail (Just Visiting)"),
-        new Property("St. Charles Place", "Pink", 140, 10),
-        new Commodity("Electric Company", "Utility", 150, 0),
-        new Property("States Avenue", "Pink", 140, 10),
-        new Property("Virginia Avenue", "Pink", 160, 12),
-        new Commodity("Pennsylvania Railroad", "Railroad", 200, 25),
-        new Property("St. James Place", "Orange", 180, 14),
+        new Property("St. Charles Place", 140, 10),
+        new Commodity("Electric Company", 150, 0),
+        new Property("States Avenue", 140, 10),
+        new Property("Virginia Avenue", 160, 12),
+        new Commodity("Pennsylvania Railroad", 200, 25),
+        new Property("St. James Place", 180, 14),
         new CommunityChest("Community Chest"),
-        new Property("Tennessee Avenue", "Orange", 180, 14),
-        new Property("New York Avenue", "Orange", 200, 16),
+        new Property("Tennessee Avenue", 180, 14),
+        new Property("New York Avenue", 200, 16),
         new Chance("Free Parking"),
-        new Property("Kentucky Avenue", "Red", 220, 18),
+        new Property("Kentucky Avenue",220, 18),
         new Chance("Chance"),
-        new Property("Indiana Avenue", "Red", 220, 18),
-        new Property("Illinois Avenue", "Red", 240, 20),
-        new Commodity("B&O Railroad", "Railroad", 200, 25),
-        new Property("Atlantic Avenue", "Yellow", 260, 22),
-        new Property("Ventnor Avenue", "Yellow", 260, 22),
-        new Commodity("Water Works", "Utility", 150, 0),
-        new Property("Marvin Gardens", "Yellow", 280, 24),
+        new Property("Indiana Avenue", 220, 18),
+        new Property("Illinois Avenue",240, 20),
+        new Commodity("B&O Railroad", 200, 25),
+        new Property("Atlantic Avenue", 260, 22),
+        new Property("Ventnor Avenue", 260, 22),
+        new Commodity("Water Works", 150, 0),
+        new Property("Marvin Gardens", 280, 24),
         new Chance("Go to Jail"),
-        new Property("Pacific Avenue", "Green", 300, 26),
-        new Property("North Carolina Avenue", "Green", 300, 26),
+        new Property("Pacific Avenue", 300, 26),
+        new Property("North Carolina Avenue", 300, 26),
         new CommunityChest("Community Chest"),
-        new Property("Pennsylvania Avenue", "Green", 320, 28),
-        new Commodity("Short Line Railroad", "Railroad", 200, 25),
+        new Property("Pennsylvania Avenue", 320, 28),
+        new Commodity("Short Line Railroad", 200, 25),
         new Chance("Chance"),
-        new Property("Park Place", "Blue", 350, 35),
-        new Commodity("Luxury Tax", "Tax", 100, 0),
-        new Property("Boardwalk", "Blue", 400, 50)
+        new Property("Park Place", 350, 35),
+        new Commodity("Luxury Tax", 100, 0),
+        new Property("Boardwalk", 400, 50)
     };
 };
